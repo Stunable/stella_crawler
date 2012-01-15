5b19340e11a45b3aff4028b620790f0f83681a99
Mishal Awadah <a.mamish@gmail.com>
2012-01-14 20:38:46 -0500

    created the basic stella item

--- stella_crawler/items.py
+++ stella_crawler/items.py
@@ -6,6 +6,36 @@
 from scrapy.item import Item, Field
 
 class StellaCrawlerItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+    # All items have these fields
+    brand = Field()
+    price = Field()
+    fabric = Field()
+    color = Field()          # There may be more than one color
+    num_colors = Field()
+    length = Field()
+    fabrication = Field()
+    embellishment = Field()
+    description = Field()
+    name = Field()           # The name of the item
+    print_style = Field()    # The print
+    
+    # Specific fields that only apply to certain items
+    wash = Field()           # Denim
+    rise = Field()           # Denim, Pants
+    fit = Field()            # Denim 
+    distressing = Field()    # Denim
+    neckline = Field()       # Tops, dresses, sweaters
+    sleeves = Field()
+    guage_weight = Field()
+    sheer_level = Field()
+    lining = Field()
+    waist_rise = Field()
+    padding = Field()
+    trim = Field()
+
+    # These define the item's type
+    category = Field()
+    subcategory = Field()
+
+    # The item's image(s)
+    image_urls = Field()
